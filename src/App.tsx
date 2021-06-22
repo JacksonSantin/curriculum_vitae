e2a26b3169db45c91a3cd2ac34a93b164b0dcc36
JacksonSantin <jackdhanyelsn@gmail.com>
2021-06-21 22:45:10 -0300

    Curriculum Vitae

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import ScrollButton from './components/ScrollButton';
 
 import Routes from './routes';
 
+import ReactGa from 'react-ga';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('UA-199994254-1')
+    ReactGa.pageview('/')
+  }, [])
+
   return (
     <div className="App">
       <Routes />
